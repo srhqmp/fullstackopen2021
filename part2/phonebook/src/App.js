fe81ddbe236fca01cbb1d26071a4e252c81208e0
srhqmp <ballanosarah@gmail.com>
2021-05-28 00:14:37 +0800

    solution to 2.8: the phonebook step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,18 +2,23 @@ import React, { useState } from 'react'
 
 const DisplayContact = ({contact}) => {
   return (
-    <div>{contact.name}</div>
+    <div>{contact.name} {contact.number}</div>
   )
 }
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' },
+    { name: 'Arto Hellas', number: '011101010100010100101' },
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [newNumber, setNewNumber ] = useState('') 
 
   const handleNameInput = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberInput = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const contactExists = (name) => persons.some(person => person.name === name)
@@ -22,6 +27,7 @@ const App = () => {
     event.preventDefault()
     const newContact = {
       name: newName,
+      number: newNumber,
     }
 
     if (contactExists(newName)) {
@@ -29,6 +35,7 @@ const App = () => {
     } else {
       setPersons(persons.concat(newContact))
       setNewName('')
+      setNewNumber('')
     } 
   }
 
@@ -36,9 +43,8 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form>
-        <div>
-          name: <input value={newName} onChange={handleNameInput} />
-        </div>
+        <div>name: <input value={newName} onChange={handleNameInput} /></div>
+        <div>number: <input value={newNumber} onChange={handleNumberInput} /></div>
         <div>
           <button type="submit" onClick={handleAddContact} >add</button>
         </div>
