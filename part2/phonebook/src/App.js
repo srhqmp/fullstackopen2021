5f9c8db088ccf1c25eaca2f85d30e56931ad0b56
srhqmp <ballanosarah@gmail.com>
2021-05-28 03:21:01 +0800

    solution to 2.9*: the phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,33 @@
 import React, { useState } from 'react'
 
-const DisplayContact = ({contact}) => {
+const Contact = ({person}) => {
   return (
-    <div>{contact.name} {contact.number}</div>
+    <div>{person.name} {person.number}</div>
+  )
+}
+
+const DisplayContacts = ({persons}) => {
+  return (
+    persons.map(person => <Contact key={person.name} person={person} />)
   )
 }
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '011101010100010100101' },
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'd', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
-  const [newNumber, setNewNumber ] = useState('') 
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ newSearch, setNewSearch ] = useState('')
+
+  const contactExists = (name) => persons.some(person => person.name === name)
+
+  const handleSearch = (event) => {
+    setNewSearch(event.target.value)
+  }
 
   const handleNameInput = (event) => {
     setNewName(event.target.value)
@@ -21,8 +37,6 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
-  const contactExists = (name) => persons.some(person => person.name === name)
-  
   const handleAddContact = (event) => {
     event.preventDefault()
     const newContact = {
@@ -41,8 +55,12 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        filter shown with <input value={newSearch} onChange={handleSearch} />
+      </div>
       <form>
+        <h2>Add new</h2>
         <div>name: <input value={newName} onChange={handleNameInput} /></div>
         <div>number: <input value={newNumber} onChange={handleNumberInput} /></div>
         <div>
@@ -51,7 +69,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <DisplayContact key={person.name} contact={person} />)}
+        <DisplayContacts persons={persons.filter(person => {
+          return person.name.toLocaleLowerCase().includes(newSearch.toLocaleLowerCase())
+        })} />
       </div>
     </div>
   )
