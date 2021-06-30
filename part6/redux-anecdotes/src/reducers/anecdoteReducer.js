362eb51dd4678e8b2bb73353b35147ccd6a4458f
srhqmp <ballanosarah@gmail.com>
2021-06-30 14:54:17 +0800

    solution for 6.15 - convert initializeAnecdote  to asynchronous action creator

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,7 +1,12 @@
-export const initializeAnecdotes = (anecdotes) => {
-  return {
-    type: 'INIT_ANECDOTES',
-    data: anecdotes,
+import anecdoteService from '../services/anecdotes'
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes,
+    })
   }
 }
 
