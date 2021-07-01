158608b31fdc778b21c6a1e61ba646105d3d962e
srhqmp <ballanosarah@gmail.com>
2021-07-01 10:37:35 +0800

    solution 6.19 - modify Notification to use connect function instead of hooks

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector((state) => state.notification)
+const Notification = (props) => {
+  const notification = props.notification
 
   const style = {
     display: notification.display,
@@ -14,4 +14,11 @@ const Notification = () => {
   return <div style={style}>{notification.message}</div>
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification,
+  }
+}
+
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+export default ConnectedNotification
