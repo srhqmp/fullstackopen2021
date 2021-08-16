921c42c9ff53ca60e27b5e89e6a5122d7126ecf1
srhqmp <ballanosarah@gmail.com>
2021-08-16 21:50:52 +0800

    lecture notes - first steps with typescript

---
+++ part9/samples/calculator.ts
@@ -0,0 +1,36 @@
+type Operation = 'multiply' | 'add' | 'divide'
+type Return = number | string
+
+const calculator = (a: number, b: number, op: Operation): Return => {
+    switch (op) {
+        case 'multiply':
+            return a * b
+        case 'divide':
+            if (b === a) throw new Error('Can\'t divide by 0!')
+            return a / b
+        case 'add':
+            return a + b
+        default:
+            throw new Error('Operation is not multiplu, add or divide!')
+
+    }
+}
+
+try {
+    console.log(calculator(0, 1, 'divide'))
+} catch (e) {
+    console.log('Something went wrong, error message:', e.message)
+}
+
+console.log(process.argv)
+
+// const calculator = (a: number, b: number, op: Operation): Return => {
+//     if (op === 'multiply') {
+//         return a * b
+//     } else if (op === 'add') {
+//         return a + b
+//     } else if (op === 'divide') {
+//         if (b === 0) return 'can\'t divide by 0!'
+//         return a / b
+//     }
+// }
